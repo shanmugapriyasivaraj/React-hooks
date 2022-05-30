1234890dc8b4892c524b96b7ec51bd66ad898577
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-30 22:16:48 +0530

    Use Effect

--- src/App.js
+++ src/App.js
@@ -1,62 +1,107 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
+// import "./App.css";
+
+// function App() {
+//   const [name, setName] = useState("Varun");
+//   const [flag, setFlag] = useState(false);
+//   const [steps, setSteps] = useState(0);
+//   const [names, setNames] = useState([]);
+
+//   const changeName = () => {
+//     console.log("Clicked");
+//     // return setName("Nithya");
+//     return setFlag(!flag);
+//   };
+
+//   const increment = () => {
+//     console.log("increased");
+//     return setSteps(steps + 1);
+//   };
+
+//   const decrement = () => {
+//     console.log("decreased");
+//     return setSteps(steps - 1);
+//   };
+
+//   const addNames = (e) => {
+//     e.preventDefault();
+//     setNames([...names, { id: names.length, name }]);
+//     setName("");
+//   };
+
+//   return (
+//     <div className="App">
+//       <div>Hello {flag ? name : ""}</div>
+//       <button onClick={changeName}>Click me</button>
+//       <hr></hr>
+//       <div>{steps}</div>
+//       <button onClick={increment}>+</button>
+//       <button onClick={decrement}>-</button>
+//       <hr></hr>
+//       <form onSubmit={addNames}>
+//         <input
+//           type="text"
+//           placeholder="add Names"
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//         />
+//         <br />
+//         <button>Add</button>
+//       </form>
+//       <hr></hr>
+//       <ul>
+//         {names.map((item) => (
+//           <li key={item.id}>{item.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  const [name, setName] = useState("Varun");
-  const [flag, setFlag] = useState(false);
-  const [steps, setSteps] = useState(0);
-  const [names, setNames] = useState([]);
+const App = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [name, setName] = useState("");
 
-  const changeName = () => {
-    console.log("Clicked");
-    // return setName("Nithya");
-    return setFlag(!flag);
-  };
+  // On every Render
+  useEffect(() => {
+    console.log("I re-rendered");
+  });
 
-  const increment = () => {
-    console.log("increased");
-    return setSteps(steps + 1);
-  };
+  // On first Render/Mount Only
+  useEffect(() => {
+    console.log(" The component mounted");
+  });
 
-  const decrement = () => {
-    console.log("decreased");
-    return setSteps(steps - 1);
-  };
+  // On first render+ whenever dependency changes
 
-  const addNames = (e) => {
-    e.preventDefault();
-    setNames([...names, { id: names.length, name }]);
-    setName("");
-  };
+  useEffect(() => {
+    console.log(`The name changed:${name}`);
+  }, [name]);
+
+  // component will unmount
+  useEffect(() => {
+    window.addEventListener("resize", setWindowWidth);
+  });
 
   return (
-    <div className="App">
-      <div>Hello {flag ? name : ""}</div>
-      <button onClick={changeName}>Click me</button>
-      <hr></hr>
-      <div>{steps}</div>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-      <hr></hr>
-      <form onSubmit={addNames}>
+    <div className="app">
+      <center>
+        <h1>USE EFFECT</h1>
+        <h2>The Window Width is : {windowWidth}</h2>
         <input
-          type="text"
-          placeholder="add Names"
           value={name}
           onChange={(e) => setName(e.target.value)}
+          placeholder="Enter a Name"
         />
-        <br />
-        <button>Add</button>
-      </form>
-      <hr></hr>
-      <ul>
-        {names.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
+      </center>
     </div>
   );
-}
+};
 
 export default App;
